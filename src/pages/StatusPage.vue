fb05653e7d3f3b21b8649d53c2346cc464a1624c
vivekjaiswar <vivekjaiswar404@gmail.com>
2026-06-05 17:58:29 +0530

    Replace status page header with ZMonitor

--- src/pages/StatusPage.vue
+++ src/pages/StatusPage.vue
@@ -571,7 +571,7 @@
                 <p v-if="config.showPoweredBy" data-testid="powered-by">
                     {{ $t("Powered by") }}
                     <a target="_blank" rel="noopener noreferrer" href="https://github.com/louislam/uptime-kuma">
-                        {{ $t("Uptime Kuma") }}
+                        {{ $t("ZMonitor") }}
                     </a>
                 </p>
 
